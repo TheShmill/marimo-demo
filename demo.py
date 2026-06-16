5c4c19b939e0a0242116fdfa48bf1cae7fd70c48
TheShmill <21013808+TheShmill@users.noreply.github.com>
2026-06-16 09:57:52 -0400

    Add demo.py

---
+++ demo.py
@@ -0,0 +1,38 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "marimo>=0.23.3",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.23.9"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    x = 10
+    return (x,)
+
+
+@app.cell
+def _():
+    y = 5
+    return (y,)
+
+
+@app.cell
+def _(x, y):
+    x + y
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
