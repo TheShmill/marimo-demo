9160a1f7f9b3aec5296157f8b88a085aeeaacea9
TheShmill <21013808+TheShmill@users.noreply.github.com>
2026-06-16 10:40:15 -0400

    Add a bit of word analysis

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@
 # requires-python = ">=3.13"
 # dependencies = [
 #     "marimo>=0.23.3",
+#     "pandas>=3.0.3",
+#     "requests>=2.34.2",
 # ]
 # ///
 
@@ -9,6 +11,16 @@ import marimo
 
 __generated_with = "0.23.9"
 app = marimo.App()
+
+
+@app.cell
+def _():
+    import requests
+    import pandas as pd
+    import numpy as np
+    import re
+
+    return pd, re, requests
 
 
 @app.cell
@@ -26,6 +38,41 @@ def _():
 @app.cell
 def _(x, y):
     x + y
+    return
+
+
+@app.cell
+def _(re, requests):
+    body = requests.get("https://www.gutenberg.org/cache/epub/2701/pg2701.txt").text.lower()
+    body = re.sub("[^a-z]", " ", body)
+    body
+    return (body,)
+
+
+@app.cell
+def _(body, pd):
+    words = pd.DataFrame({"words": body.split()})
+    words
+    return (words,)
+
+
+@app.cell
+def _(words):
+    most_frequent_words = words.groupby("words").size().sort_values(ascending=False)
+    most_frequent_words
+    return
+
+
+@app.cell
+def _(words):
+    long_words = words[words["words"].str.len() > 8]
+    long_words
+    return (long_words,)
+
+
+@app.cell
+def _(long_words):
+    long_words.groupby("words").size().sort_values(ascending=False)
     return
 
 
